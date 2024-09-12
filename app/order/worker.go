b37874b63205d6936735e770b2735894de873041
Prathyush PV <prathyush.pv@temporal.io>
2024-09-12 10:21:39 -0700

    Worker tuning

--- app/order/worker.go
+++ app/order/worker.go
@@ -6,21 +6,12 @@ import (
 	"github.com/temporalio/reference-app-orders-go/app/config"
 	"github.com/temporalio/reference-app-orders-go/app/temporalutil"
 	"go.temporal.io/sdk/client"
-	"go.temporal.io/sdk/contrib/resourcetuner"
 	"go.temporal.io/sdk/worker"
 )
 
 // RunWorker runs a Workflow and Activity worker for the Order system.
 func RunWorker(ctx context.Context, config config.AppConfig, client client.Client) error {
-	tuner, err := resourcetuner.NewResourceBasedTuner(resourcetuner.ResourceBasedTunerOptions{
-		TargetMem: 0.7,
-		TargetCpu: 0.7,
-	})
-	if err != nil {
-		return err
-	}
-
-	w := worker.New(client, TaskQueue, worker.Options{Tuner: tuner})
+	w := worker.New(client, TaskQueue, worker.Options{})
 
 	w.RegisterWorkflow(Order)
 	w.RegisterActivity(&Activities{BillingURL: config.BillingURL, OrderURL: config.OrderURL})
@@ -30,15 +21,7 @@ func RunWorker(ctx context.Context, config config.AppConfig, client client.Clien
 
 // RunBatchOrderWorker runs a Workflow and Activity worker for the BatchOrder system.
 func RunBatchOrderWorker(ctx context.Context, config config.AppConfig, client client.Client) error {
-	tuner, err := resourcetuner.NewResourceBasedTuner(resourcetuner.ResourceBasedTunerOptions{
-		TargetMem: 0.7,
-		TargetCpu: 0.7,
-	})
-	if err != nil {
-		return err
-	}
-
-	w := worker.New(client, TaskQueueBatchOrders, worker.Options{Tuner: tuner})
+	w := worker.New(client, TaskQueueBatchOrders, worker.Options{})
 
 	w.RegisterWorkflow(BatchOrders)
 	w.RegisterActivity(&Activities{BillingURL: config.BillingURL, OrderURL: config.OrderURL, TemporalClient: client})
--- app/shipment/worker.go
+++ app/shipment/worker.go
@@ -6,7 +6,6 @@ import (
 	"github.com/temporalio/reference-app-orders-go/app/config"
 	"github.com/temporalio/reference-app-orders-go/app/temporalutil"
 	"go.temporal.io/sdk/client"
-	"go.temporal.io/sdk/contrib/resourcetuner"
 	"go.temporal.io/sdk/worker"
 )
 
@@ -17,15 +16,7 @@ type Config struct {
 
 // RunWorker runs a Workflow and Activity worker for the Shipment system.
 func RunWorker(ctx context.Context, config config.AppConfig, client client.Client) error {
-	tuner, err := resourcetuner.NewResourceBasedTuner(resourcetuner.ResourceBasedTunerOptions{
-		TargetMem: 0.7,
-		TargetCpu: 0.7,
-	})
-	if err != nil {
-		return err
-	}
-
-	w := worker.New(client, TaskQueue, worker.Options{Tuner: tuner})
+	w := worker.New(client, TaskQueue, worker.Options{})
 
 	w.RegisterWorkflow(Shipment)
 	w.RegisterActivity(&Activities{ShipmentURL: config.ShipmentURL})
