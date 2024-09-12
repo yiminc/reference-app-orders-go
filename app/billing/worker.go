950741766c5a0a3e07776e14c80ce85e67ecec55
Prathyush PV <prathyush.pv@temporal.io>
2024-09-12 10:27:58 -0700

    Tuning

--- app/billing/worker.go
+++ app/billing/worker.go
@@ -6,12 +6,21 @@ import (
 	"github.com/temporalio/reference-app-orders-go/app/config"
 	"github.com/temporalio/reference-app-orders-go/app/temporalutil"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/contrib/resourcetuner"
 	"go.temporal.io/sdk/worker"
 )
 
 // RunWorker runs a Workflow and Activity worker for the Billing system.
 func RunWorker(ctx context.Context, config config.AppConfig, client client.Client) error {
-	w := worker.New(client, TaskQueue, worker.Options{})
+	tuner, err := resourcetuner.NewResourceBasedTuner(resourcetuner.ResourceBasedTunerOptions{
+		TargetMem: 0.9,
+		TargetCpu: 0.9,
+	})
+	if err != nil {
+		return err
+	}
+
+	w := worker.New(client, TaskQueue, worker.Options{Tuner: tuner})
 
 	w.RegisterWorkflow(Charge)
 	w.RegisterActivity(&Activities{FraudCheckURL: config.FraudURL})
